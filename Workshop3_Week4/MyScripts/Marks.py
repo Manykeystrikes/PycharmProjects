183f1c2a902f2d89f0bdf0ccaa5d0e70b0d55809
Manykeystrikes <adbart@live.com.au>
2024-08-17 13:08:16 +1000

    update

--- Workshop3_Week4/MyScripts/Marks.py
+++ Workshop3_Week4/MyScripts/Marks.py
@@ -1,3 +1,5 @@
+#Average Mark
+
 '''Test Results
 Problem: Prompt for and read marks for a test until a negative
 number is entered (which can not be a valid mark). Print the
@@ -7,13 +9,18 @@ Problem: Extend the problem so that it prints out the highest
 and lowest marks as well.'''
 
 n = 0
-total = 10.5
-mark = float(input("Enter a mark: "))
+total = 0.0
+mark = float(input("Enter a mark: ",))
+highest = mark
+lowest = mark
 while mark >= 0.0:
    n += 1
    total += mark
-   mark = float(input("Enter a mark: "))
-
-print("Marks recieved: ",n)
+   if mark > highest:
+       highest = mark
+   if mark < lowest:
+       lowest = mark
+   mark = float(input("Enter a mark: ", ))
+print("The number of marks: ", n)
 if n > 0:
-   print("The average mark is: ", total / n)
+   print(f"The average mark is: {round(total / n, 2)}")
