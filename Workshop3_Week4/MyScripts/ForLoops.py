1c78d5494460405cb0f04da85ad268fd6fe49d31
Manykeystrikes <adbart@live.com.au>
2024-08-17 23:43:05 +1000

    update

---
+++ Workshop3_Week4/MyScripts/ForLoops.py
@@ -0,0 +1,10 @@
+#line.py
+#print a line of hashes
+
+n = int(input("Enter a number: "))
+for i in range(n):
+   print("#", end = "") # end = "" will place all
+                        # outputs on the same line.
+print()
+   
+   
