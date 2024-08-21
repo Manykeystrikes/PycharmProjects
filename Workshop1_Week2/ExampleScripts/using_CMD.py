43d32a173ec3951110d9ff6b9e37e8a0a66325b5
Manykeystrikes <adbart@live.com.au>
2024-08-21 10:55:31 +1000

    update

---
+++ Workshop1_Week2/ExampleScripts/using_CMD.py
@@ -0,0 +1,10 @@
+print("Use_Scripts")
+
+
+C:\Users\adbar\PycharmProjects\Programming Principles(1811ICT)\Workshop1_Week2>cd ExampleScripts
+
+C:\Users\adbar\PycharmProjects\Programming Principles(1811ICT)\Workshop1_Week2\ExampleScripts>echo print("Use_Scripts")>using_CMD.py
+
+
+C:\Users\adbar\PycharmProjects\Programming Principles(1811ICT)\Workshop1_Week2\ExampleScripts>Notepad using_CMD.py
+
