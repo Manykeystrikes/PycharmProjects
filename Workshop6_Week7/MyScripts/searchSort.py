bbece1e5277b14612c47f00a8e00ce992b7099fd
Manykeystrikes <adbart@live.com.au>
2024-09-12 12:01:50 +1000

    update

--- Workshop6_Week7/MyScripts/searchSort.py
+++ Workshop6_Week7/MyScripts/searchSort.py
@@ -1,7 +1,7 @@
 # searchSort
 """Functions for searching and sorting"""
 def sort(xs):
-    """Sort list xs in place into non-descending order."""
+    """Sort list xs in place into non-descending order.
    for i in range(0, len(xs) - 1):
        for j in range(i + 1, len(xs)):
            if xs[j] < xs[i]:
